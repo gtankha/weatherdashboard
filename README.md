7a5adb31f6c96e3ad83ef1a45fac8136951c4e51
gtankha <46304828+gtankha@users.noreply.github.com>
2020-08-28 15:10:06 -0700

    completed version

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # weatherdashboard
+
+### Main functions are as followa:
+###  getSearchCity -- Grabs the city name from search submission and initiates the process of gathering and displaying weather data by calling other functions
+### getWeatherValues --- Grabs the objects from the external openweathermap APIs 
+
+### setSeachHistory -- set search history and local storage based on searched city. This function is only called if the city is not already part of the search history. All search history cities are clickable to initate a search.
+
+### displayWeather - displays the current weather conditions based on parameters gathered by getWeatherValues. Note that the UV value information is gatheres from another openweathermap API
+
+### displayWeatherForecast - displays the 5 day forecast weather based on parameters gathered by getWeatherValues
+
+### There is an event listener for click on any search history city
+
+### Limited search history to 8 cities/searches. Consequent searches overwrite the existing search history. 
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,8 @@
  var formCityEl = document.querySelector("#citys");
  var forecastNowEl = document.querySelector("#forecastNow");
  var appId = "e4817a0deb146d8c3d95dc704d60b57c";
- var a = 0;
- var city = ["","","","","","","",""];
+ var a = 0; // global variable to keep track of city array index for search history 
+ var city = ["","","","","","","",""]; // global array of search history cities 
  $(".5day").hide();
 
  // initialize search history
@@ -21,20 +21,22 @@ if (city1) {
       }
 };
 
+// Get the search city 
  var getSearchCity = function (event) {
 
     event.preventDefault();
     // get value from input element .. the city name
     var searchString = formCityEl.value.trim();
-
+    // get weather parameters based on search city
     getWeatherValues(searchString);
-
 
  }
  
+ // get weather parameters based on the search city
 var getWeatherValues = function (citys) {
  // format the weather map api url
  
+// URLs for the current weather forecast and future forecast 
  var apiUrl = ("http://api.openweathermap.org/data/2.5/weather?q=" + citys + "&appid="+ appId + "&units=imperial");
  var apiUrlfcst = ("http://api.openweathermap.org/data/2.5/forecast?q="+ citys +"&appid="+ appId + "&units=imperial");
 // }
@@ -43,8 +45,10 @@ var getWeatherValues = function (citys) {
    // request was successful
    if (response.ok) {
      response.json().then(function(data) {
+       //display the weather data 
        displayWeather (data); 
       if (!(city.includes(citys))) {
+       // set search history  
        setSearchHistory(citys);
       }
      });
@@ -62,6 +66,7 @@ var getWeatherValues = function (citys) {
    // request was successful
    if (response2.ok) {
      response2.json().then(function(data2) {
+       // display the 5 day forecast
        displayWeatherForecast (data2);
      });
    } else {
@@ -75,27 +80,22 @@ var getWeatherValues = function (citys) {
 
 }
 
+// set the search history and set the local storage
 var setSearchHistory = function (search) {
 
- 
+ // global array index to keep track of city history
   if (a > 7) { a = 0};
   if (city) {
     
-
-
     if (city[a]) { 
-
-      var b = document.getElementById("0");
-    
       
      var val = (document.getElementById(JSON.stringify(a)));
-    
      val.textContent = search;
      city[a] = search;
 
     }
    else { 
-    
+    // display search to screen
     city[a] = search;
     $(".searchHistory").append('<li class = "history list-group-item" id ="'+JSON.stringify(a)+'">'+city[a]+'</li>');
     
@@ -111,12 +111,13 @@ var setSearchHistory = function (search) {
   }
       
     
-  
+ // add city to local storage  
   a++;
   localStorage.setItem("city",JSON.stringify(city));
 
 }
 
+// display the 5 day weather forecast
 var displayWeatherForecast = function(wfcast) {
   
   
@@ -125,7 +126,7 @@ var displayWeatherForecast = function(wfcast) {
   for (i=1; i<6 ; i++) {
     $("#forecast"+i.toString()).empty();
   }
-
+// get the forecast parameters 
  for (i=1; i<6 ; i++) {
   var date = wfcast.list[(8*(i-1))+1].dt; 
   date = parseInt (date);
@@ -133,6 +134,7 @@ var displayWeatherForecast = function(wfcast) {
   var weathericon = wfcast.list[(8*(i-1))+1].weather[0].icon ;
   var iconurl = "http://openweathermap.org/img/wn/" + weathericon + "@2x.png";
   
+  // display the forecast parameters
    $("#forecast"+i.toString()).append('<p> <strong>'+ date + '</strong> </p>');
    $("#forecast"+i.toString()).append('<img class = "w-75" id="weathericon2" src="' +iconurl+'" alt="weathericon">');
    $("#forecast"+i.toString()).append('<p>'+ wfcast.list[(8*(i-1))+1].main.temp + 'F </p>');
@@ -141,6 +143,7 @@ var displayWeatherForecast = function(wfcast) {
 
 }
 
+// display the weather parameters
  var displayWeather = function (weather){
 
     var date = weather.lastupdate;
@@ -157,6 +160,7 @@ var displayWeatherForecast = function(wfcast) {
     $("#forecastNow").append(' <p> Wind Speed: ' + weather.wind.speed + 'MPH </p>' );
     var lat = weather.coord.lat;
     var lon = weather.coord.lon;
+    // display UV index which is a seperate API
     var apiUrl2 = ("http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid="+appId);
     // }
      fetch(apiUrl2)
@@ -164,6 +168,7 @@ var displayWeatherForecast = function(wfcast) {
        // request was successful
        if (response.ok) {
          response.json().then(function(data2) {
+           // Color UV index according to value
           if (data2.value > 8) {var color = "bg-danger";}
           if (data2.value > 4 && data2.value < 8) {var color = "bg-warning";}
           if (data2.value > 0 && data2.value < 4) {var color = "bg-success";}
@@ -181,9 +186,10 @@ var displayWeatherForecast = function(wfcast) {
     
  }
 
-
+// city search input
 $("#citySearch").submit(getSearchCity);
 
+// click on search history
 $(".searchHistory").click(function(e) {
 var hist = e.target.id;
 hist = document.getElementById(hist);
